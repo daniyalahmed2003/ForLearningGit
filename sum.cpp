e9a9261cec1e99a47e1a2dd9c822f21017e30faf
Daniyal Ahmed <daniyalahmad302003@gmail.com>
2022-10-02 21:14:29 +0500

    I am doing this for learning

--- sum.cpp
+++ sum.cpp
@@ -6,6 +6,6 @@ int main()
     int a=2,b=3,sum;
     sum=a+b;
     cout<<"Sum = "<<sum;
-    cout<<"\nI am doing this for learning git again";
+    cout<<"\nI am doing this for learning git again dfdsfdsfdfs dsfsf 34 fs4";
     return 0;
 }
